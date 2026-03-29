b27450a80ed8a4dccf6bcb9b50d1ca7bc158e28b
Nölli_Hestur <nokkvi@emblaognokkvi.com>
2026-03-29 10:50:02 +0000

    bætti við randomizer

--- create_zip.ps1
+++ create_zip.ps1
@@ -5,10 +5,10 @@ New-Item -ItemType Directory -Path $tempFolder | Out-Null
 Copy-Item 'dist\MacroRecorder.exe' $tempFolder
 Copy-Item 'feather_icon.ico' $tempFolder
 Copy-Item 'Install to Desktop.bat' $tempFolder
-Copy-Item 'README.txt' $tempFolder
+Copy-Item 'README.md' $tempFolder
 
 Compress-Archive -Path $tempFolder -DestinationPath 'dist\MacroRecorder.zip' -Force
 Remove-Item $tempFolder -Recurse -Force
 
 Write-Host 'Distribution package created: dist\MacroRecorder.zip'
-Write-Host 'Contents: MacroRecorder.exe, feather_icon.ico, Install to Desktop.bat, README.txt'
+Write-Host 'Contents: MacroRecorder.exe, feather_icon.ico, Install to Desktop.bat, README.md'
